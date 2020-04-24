b47370797c7b3e282e919834720006f2715d19b0
sunsdo0513 <54053277+sunsdo0513@users.noreply.github.com>
2020-04-24 19:04:56 +1000

    添加项目文件。

---
+++ Controllers/POIsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CaringSquareApp.Models;
+
+namespace CaringSquareApp.Controllers
+{
+    public class POIsController : Controller
+    {
+        private CaringSquareEntities db = new CaringSquareEntities();
+
+        // GET: POIs
+        public ActionResult Index()
+        {
+            return View(db.POIs.ToList());
+        }
+
+        public ActionResult Shopping()
+        {
+            var shoppingLists = db.POIs.Where(s => s.Category == "shopping").ToList();
+            return View(shoppingLists);
+        }
+
+        // GET: POIs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            POIs pOIs = db.POIs.Find(id);
+            if (pOIs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pOIs);
+        }
+
+        // GET: POIs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: POIs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PlaceId,Name,Theme,SubTheme,Address,Latitude,Longitude,Category")] POIs pOIs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.POIs.Add(pOIs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pOIs);
+        }
+
+        // GET: POIs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            POIs pOIs = db.POIs.Find(id);
+            if (pOIs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pOIs);
+        }
+
+        // POST: POIs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PlaceId,Name,Theme,SubTheme,Address,Latitude,Longitude,Category")] POIs pOIs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pOIs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pOIs);
+        }
+
+        // GET: POIs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            POIs pOIs = db.POIs.Find(id);
+            if (pOIs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pOIs);
+        }
+
+        // POST: POIs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            POIs pOIs = db.POIs.Find(id);
+            db.POIs.Remove(pOIs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/SocialEventsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CaringSquareApp.Models;
+using Microsoft.AspNet.Identity;
+
+namespace CaringSquareApp.Controllers
+{
+    public class SocialEventsController : Controller
+    {
+        private CaringSquareEntities db = new CaringSquareEntities();
+
+        // GET: SocialEvents
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var eventLists = db.SocialEvents.Where(s => s.UserUserId == userId).ToList();
+            //var socialEvents = db.SocialEvents.Include(s => s.AspNetUser).Include(s => s.POIs);
+            return View(eventLists);
+        }
+
+        // GET: SocialEvents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SocialEvent socialEvent = db.SocialEvents.Find(id);
+            if (socialEvent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socialEvent);
+        }
+
+        // GET: SocialEvents/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserUserId = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.POIPlaceId = new SelectList(db.POIs, "PlaceId", "Name");
+            return View();
+        }
+
+        // POST: SocialEvents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EventId,EventName,EventDate,EventTime,UserUserId,POIPlaceId")] SocialEvent socialEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SocialEvents.Add(socialEvent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserUserId = new SelectList(db.AspNetUsers, "Id", "Email", socialEvent.UserUserId);
+            ViewBag.POIPlaceId = new SelectList(db.POIs, "PlaceId", "Name", socialEvent.POIPlaceId);
+            return View(socialEvent);
+        }
+
+        // GET: SocialEvents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SocialEvent socialEvent = db.SocialEvents.Find(id);
+            if (socialEvent == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserUserId = new SelectList(db.AspNetUsers, "Id", "Email", socialEvent.UserUserId);
+            ViewBag.POIPlaceId = new SelectList(db.POIs, "PlaceId", "Name", socialEvent.POIPlaceId);
+            return View(socialEvent);
+        }
+
+        // POST: SocialEvents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EventId,EventName,EventDate,EventTime,UserUserId,POIPlaceId")] SocialEvent socialEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(socialEvent).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserUserId = new SelectList(db.AspNetUsers, "Id", "Email", socialEvent.UserUserId);
+            ViewBag.POIPlaceId = new SelectList(db.POIs, "PlaceId", "Name", socialEvent.POIPlaceId);
+            return View(socialEvent);
+        }
+
+        // GET: SocialEvents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SocialEvent socialEvent = db.SocialEvents.Find(id);
+            if (socialEvent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socialEvent);
+        }
+
+        // POST: SocialEvents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SocialEvent socialEvent = db.SocialEvents.Find(id);
+            db.SocialEvents.Remove(socialEvent);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
